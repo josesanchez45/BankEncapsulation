2a5e29e88f16c8245ee21ee1270c4a986e2217fe
jose sanchez <jose.sanchez.artist@gmail.com>
2022-08-19 08:53:57 -0700

    initial commit

---
+++ BankEncapsulation/Bank Account.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankEncapsulation
+{
+    public class Bank_Account
+    {
+        private double Balance = 0;
+
+        public double Deposit(double amount)
+        {
+            return Balance += amount;
+        }
+
+        public void GetBalance()
+        {
+            Console.WriteLine(Balance);
+        }
+
+       
+    }
+}
---
+++ BankEncapsulation/Program.cs
@@ -0,0 +1,18 @@
+﻿using BankEncapsulation;
+var account= new Bank_Account();
+Console.WriteLine($"Your current balance is: ");
+
+account.GetBalance();
+
+Console.WriteLine();
+
+Console.WriteLine("How much Would you like to deposit?");
+
+var userDeposit =double.Parse(Console.ReadLine());
+
+account.Deposit(userDeposit);
+
+Console.WriteLine("Your new balance is: ");
+
+account.GetBalance();
+
